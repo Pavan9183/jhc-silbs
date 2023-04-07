3849db8d49650d1024f58d8c8373e662a2fd4dc6
Pavan9183 <127922833+Pavan9183@users.noreply.github.com>
2023-04-07 13:01:41 +0530

    Update tamcatdeploy.groovy

--- vars/tamcatdeploy.groovy
+++ vars/tamcatdeploy.groovy
@@ -1,7 +1,7 @@
 def call(){
- sshagent(['kumar']) {
+ sshagent(['KeyPair1']) {
    sh "scp -o StrictHostKeyChecking=no target/hr-api.war ec2-user@172.31.19.147:/opt/tomcat9/webapps/"
-    sh "ssh ec2-user@172.31.19.147	 /opt/tomcat9/bin/shutdown.sh"
+    sh "ssh ec2-user@172.31.19.147 /opt/tomcat9/bin/shutdown.sh"
      sh "ssh ec2-user@172.31.19.147 /opt/tomcat9/bin/startup.sh"
    }
 }
